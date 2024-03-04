f7864d58c16f1edf9205391c6ea7eccf350d8faa
Stefano Mis <stefano.mis@plusolutions.it>
2024-03-04 12:26:48 +0100

    FIne lezione 1

--- Lezione_1/js_esempio5.js
+++ Lezione_1/js_esempio5.js
@@ -1,5 +1,15 @@
 function myFunction() {
     console.log('hello world');
+
+    const input1 = document.getElementById('input1');
+    const input2 = document.getElementById('input2');
+    const edit = document.getElementById('edit');
+
+    console.log(input1, input2, edit);
+    console.log(input1.value, input2.value);
+
+    edit.innerHTML = input1.value + ' ' + input2.value;
 }
 
-document.getElementById("my-button").addEventListener("click", myFunction);
+document.getElementById("btn").addEventListener('click', myFunction);
+
