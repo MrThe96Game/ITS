7757a05a5e881eeeb7d38fcc6cf4db6502cfb9a4
Stefano Mis <stefano.mis@plusolutions.it>
2024-04-18 12:51:52 +0200

    Lezione9

---
+++ Lezione9/my-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <RowWord :word="myArray"/>
+</template>
+
+<script>
+import RowWord from "./RowWord.vue";
+
+export default {
+  components: {
+    RowWord
+  },
+  data: function() {
+    return {
+      myArray: []
+    }
+  },
+  created() {
+    window.addEventListener('keydown', (e) => {
+      this.myArray.push(e.key.toString())
+    })
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Lezione9/my-project/src/RowWord.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="sub-el" v-for="index in 5">
+            {{ word[index - 1] }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [ 'word' ]
+}
+</script>
+
+<style>
+.container {
+    display: flex;
+}
+
+.sub-el {
+    width: 30px;
+    height: 30px;
+    border: 2px solid #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    font-weight: bold;
+}
+</style>
